00ce058b9c34215dd0cf2beca4acb3fcdfa63ed2
kwan528 <kwan528@aucklanduni.ac.nz>
2021-04-09 23:52:14 +1200

    styling
    styling for main page

---
+++ Chronicle/src/components/Main.jsx
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from 'react';
+import { Link, useLocation } from 'react-router-dom';
+import { Formik, Field, Form, ErrorMessage, yupToFormErrors, useFormik } from 'formik';
+import './main.css';
+export function Main(props) {
+
+return (
+    <body  style={{ backgroundImage: "url(/background.jpg)" }}>    
+        <Formik>
+    {({ errors, touched, isSubmitting }) => (
+        <Form>
+            <div>   
+                <div>
+                    <h1 class="main_h1">Welcome! </h1>
+                        <h1 class="main_h1">Easy DD got all you need for investing</h1>
+                </div>
+                <div id="btn">
+                    <Link to="/Result">
+                        <button class="big-button">
+                            Check out the market
+                        </button>
+                    </Link>
+                </div>
+                <div>
+                </div>
+            </div>
+        </Form>
+    )}
+</Formik>
+</body>
+
+)
+}
