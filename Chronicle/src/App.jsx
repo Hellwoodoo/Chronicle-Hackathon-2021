2ccfa47bb8a3e3646a52e27ecabb6c612fbf87ab
Eddie Xu <edward.xxu@gmail.com>
2021-04-09 15:03:38 +1000

    Adding reactJS

---
+++ Chronicle/src/App.jsx
@@ -0,0 +1,20 @@
+import React, { useState, useEffect } from 'react';
+import { Route, Switch, BrowserRouter, Redirect, useLocation } from 'react-router-dom';
+import { Main } from './components/Main';
+import { Result } from './components/Result';
+
+
+function App() {
+  return (
+    <div className='app-container'> 
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" component={Main} exact />
+          <Route path="/Result" component={Result} />
+
+        </Switch>
+      </BrowserRouter>              
+    </div>
+  )
+}
+export default App;
