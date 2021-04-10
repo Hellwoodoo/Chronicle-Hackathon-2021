aa0244c322dc7b50638b5029bf0b691653a7da80
kwan528 <kwan772@aucklanduni.ac.nz>
2021-04-11 10:19:01 +1200

    add tooltip
    tooltips for each symbol

--- Chronicle/src/components/CompanyList.jsx
+++ Chronicle/src/components/CompanyList.jsx
@@ -17,10 +17,14 @@ const CompanyList = ({companyList=[]}) => {
           return (
             <tr>
               <td>{data.name}</td>
-              <td><i class="em em-rocket" aria-role="presentation" aria-label="ROCKET"></i></td>
-              <td><img class="symbol_img" src="/diamond_hand.png" alt="diamond hand"/></td>
-              <td><img class="symbol_img" src="/red_arrow.png" alt="red arrow"/></td>
-              <td></td>
+              <td><figure class="r"><i class="em em-rocket" aria-role="presentation" aria-label="ROCKET"></i>
+              <span class="tip_r">Explosive growth</span></figure></td>
+              <td><figure class="dh"><img class="diamond_hand" src="/diamond_hand.png" alt="diamond hand"/>
+              <span class="tip_dh">Consistent growth</span></figure></td>
+              <td><figure class="ra"><img class="red_arrow" src="/red_arrow.png" alt="red arrow"/>
+              <span class="tip_ra">Massive loss</span></figure></td>
+              <td><figure class="ph"><i class="em em-raised_back_of_hand" aria-role="presentation" aria-label="RAISED BACK OF HAND"></i>
+              <span class="tip_ph">Consistent loss</span></figure></td>
               <td></td>
       </tr>	
     	   )	
